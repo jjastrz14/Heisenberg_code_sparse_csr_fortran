2d4c3a637b60c7e583b6e64913a07964fc11ddff
Jakub Jastrzebski <jakub.jastrzebski99@gmail.com>
2024-02-27 15:56:18 +0100

    lets come back

--- spin_systems_module.f90
+++ spin_systems_module.f90
@@ -1,6 +1,306 @@
 module spin_systems
     contains
 
+    subroutine Sz_subspace_choice(N_spin, Sz_choice, hash_Sz, Sz_subspace_size)
+        implicit none
+        integer, intent(in) :: N_spin
+        double precision, intent(in) :: Sz_choice
+        integer, intent(out) :: Sz_subspace_size
+        integer, allocatable :: hash_Sz(:)
+        
+        integer :: N_spin_max, i, j, ind_2, Sz_choice_ind
+        double precision :: Sz
+        logical :: bool
+        
+        !double precision, allocatable   :: Sz_basis(:)
+        !info
+        !
+        !CHARACTER*1 :: id
+
+        N_spin_max = 2**N_spin
+        
+        !from all N_spin_max we should be able to predict degeneracy of restricted S_z
+        !1) we need to know Sz_choice_ind from Sz_choice and N_spin_max
+        !Sz_choice = N_spin_max/2      -> Sz_choice_ind = 0
+        !Sz_choice = N_spin_max/2-1    -> Sz_choice_ind = 1
+        !Sz_choice = N_spin_max/2-2    -> Sz_choice_ind = 2
+        write(*,*) 'choosen spin= ', Sz_choice
+        
+        Sz_choice_ind = int( N_spin/2.0d0 - Sz_choice )
+        write(*,*) 'calculated integer Sz_choice_ind = ', Sz_choice_ind
+        
+        !2) Sz_subspace_size = 4 here Newton symbol to be implemented = ( N_spin_max over Sz_choice_ind )
+        
+        !testing binomialCoefficient(n, k, C)
+        call binomialCoefficient(N_spin, Sz_choice_ind, Sz_subspace_size)
+        
+        !Sz_subspace_size = 4 ! standard factorial calcualated recursively/ logartihmically
+        write(*,*) 'Sz_subspace_size = ', Sz_subspace_size
+        
+        !Sz_subspace_size = 1
+        
+        allocate ( hash_Sz(Sz_subspace_size) )
+
+        ! we can use e.g. btest function, which returns info if given bit is 0 (false) or 1 (true) 
+        !bool_T/F=btest(number, bit_number)
+        ! 0 == false => Sz=Sz+1/2, 1= true> = Sz=Sz-1/2
+        ind_2 = 1
+        do i = 0 , N_spin_max-1
+            !basis(i) = i !here we want Sz from this i
+            !write(*,*) 'basis vector i =', i+1
+            Sz = 0.0d0
+            do j=0, N_spin-1
+                bool = btest(i, j) 
+                !write(*,*) j, bool
+                
+                if (bool==.FALSE.) then
+                    Sz=Sz+1.0d0/2.0d0
+                elseif (bool==.TRUE.) then
+                    Sz=Sz-1.0d0/2.0d0
+                else
+                    write(*,*) 'something wrong'
+                    error stop
+                end if                
+            end do    
+            
+            !Sz_basis(i+1) = Sz
+            if (Sz==Sz_choice) then
+                    hash_Sz(ind_2) = i+1
+                    ind_2 = ind_2 + 1
+            end if
+            
+            !write(*,*) 'for basis vector i =', i, 'Sz = ', Sz
+        end do
+        
+        write(*,*) 'internal hash_Sz size check', ind_2-1, 'vs', Sz_subspace_size
+        
+        !open (unit=40, file="hash_Sz_basis.dat", recl=512)
+        !write(*,*) 'Summary of hash_Sz basis: '
+        !do i=1,ind_2-1
+        !    write(* ,*) i, hash_Sz(i)
+        !    write(40,*) i, hash_Sz(i)
+        !end do
+        !close(40)
+    end subroutine Sz_subspace_choice
+
+
+    subroutine H_XXX_subspace_fill_and_diag_fast(N_spin, J_spin, Sz_subspace_size, hash_Sz)
+        use omp_lib
+        use mkl_vsl
+        use tests_module
+        implicit none
+        
+        integer, intent(in) :: N_spin, Sz_subspace_size
+        integer, allocatable :: hash_Sz(:), list_of_ind(:,:), list_of_ind_2(:,:), ia(:), ja(:), open_mp_counter(:)
+        logical, allocatable :: list_of_ind_bool(:)
+        double precision, intent(in) :: J_spin
+        integer :: i, j, ind_i, ind_j, N_spin_max, ind_Sz_1, ind_Sz_2, ind_3, size_of_1D_list_for_sweep, &
+                   size_of_list, ja_val_arr_size, ind_temp, ind_temp_2, omp_id, threads_max
+        double precision, allocatable :: H_full(:,:), val_arr(:)
+        double precision :: norm, H_full_ij, element_value_cutoff
+ 
+        double precision, allocatable :: x(:,:)
+        integer :: fpm(128)
+        double precision :: emin, emax
+        integer :: m0
+        double precision :: epsout
+        integer :: loop
+        double precision, allocatable :: e(:)
+        integer :: m
+        double precision, allocatable :: res(:)
+        
+        CHARACTER*1 :: jobz, range, uplo
+        
+        integer :: il, iu, ldz, liwork, lwork, info, m_eig, n
+
+        open (unit=10, file="H_eig_Sz_sub_01Dec2023.dat", recl=512)
+        ! lets rewrite our matlab code
+        ! N=4 spin-1/2 Heisenberg XXX (Jx=Jy=Jz=J) model, J=1
+
+        N_spin_max = 2**N_spin
+        write(*,*) 'be carefull about N_spin_max max size - large integer possibility'
+        write(*,*) 'test for N_spin_max', N_spin_max
+        
+        ! 1D list for fast sweep
+        size_of_list = Sz_subspace_size*(Sz_subspace_size + 1  ) / 2
+        allocate( list_of_ind(size_of_list, 2) )
+        allocate( list_of_ind_bool(size_of_list) )
+        list_of_ind_bool = .FALSE.
+        ind_3 = 1
+        do ind_Sz_1 = 1        , Sz_subspace_size
+        do ind_Sz_2 = ind_Sz_1 , Sz_subspace_size
+            list_of_ind(ind_3,1) = ind_Sz_1
+            list_of_ind(ind_3,2) = ind_Sz_2
+            ind_3 = ind_3 + 1
+        end do
+        end do
+        
+        ! fast 1D sweep calculating size of ja and val_arr vectors
+          write(*,*) 'OpenMP processors check'
+          threads_max = omp_get_max_threads()
+          write(*,*) 'OpenMP thread max num check', threads_max
+          
+          allocate(open_mp_counter(threads_max) )
+          open_mp_counter = 0
+        
+            omp_id = omp_get_thread_num()
+            !open_mp_counter(omp_id+1) = open_mp_counter(omp_id+1) + 1
+            !write(*,*) 'OpenMP thread check', omp_id
+
+        element_value_cutoff =  10.0d0**(-8.00d0) ! only larger elements are taken as non-zero
+        !ja_val_arr_size = 0
+        !$OMP PARALLEL DO PRIVATE(ind_3, omp_id, ind_Sz_1, ind_Sz_2, ind_i, ind_j, H_full_ij) &
+                          SHARED(size_of_list, list_of_ind, open_mp_counter, list_of_ind_bool, hash_Sz, N_spin, J_spin, element_value_cutoff) &
+                          default(none)
+                           
+        do ind_3 = 1, size_of_list
+            omp_id = omp_get_thread_num()
+            ind_Sz_1 = list_of_ind(ind_3,1)
+            ind_Sz_2 = list_of_ind(ind_3,2)
+            if (ind_Sz_1 == ind_Sz_2) then
+                !ja_val_arr_size = ja_val_arr_size + 1
+                open_mp_counter(omp_id+1) = open_mp_counter(omp_id+1) + 1
+                list_of_ind_bool(ind_3) = .TRUE.
+            else if (ind_Sz_2 .GT. ind_Sz_1) then 
+                ind_i = hash_Sz(ind_Sz_1)
+                ind_j = hash_Sz(ind_Sz_2)
+                call H_XXX_filling(N_spin, J_spin, ind_i, ind_j, H_full_ij)
+                if ( abs(H_full_ij) .GT. element_value_cutoff ) then
+                    !ja_val_arr_size = ja_val_arr_size + 1
+                    open_mp_counter(omp_id+1) = open_mp_counter(omp_id+1) + 1
+                    list_of_ind_bool(ind_3) = .TRUE.
+                end if
+            else
+                write(*,*) 'some error in 1D list'
+                end if
+        end do
+        !$OMP END PARALLEL DO 
+        
+        write(*,*) 'test of open_mp_counter'
+        do i = 1, threads_max
+            write(*,*) i, open_mp_counter(i)
+        end do
+        
+        ja_val_arr_size = sum(open_mp_counter)
+        write(*,*) 'necessary ja and val_arr size = ', ja_val_arr_size
+        
+        
+        do ind_3 = 1, size_of_list
+            write(*,*) ind_3, list_of_ind(ind_3,1), list_of_ind(ind_3,2), list_of_ind_bool(ind_3)                    
+        end do
+        
+        allocate( list_of_ind_2(ja_val_arr_size, 2) )
+       ! allocate( list_of_ind_bool_2(ja_val_arr_size) )
+        
+        ind_temp = 1
+        do ind_3 = 1, size_of_list
+            if( list_of_ind_bool(ind_3) ) then
+                list_of_ind_2(ind_temp, 1) = list_of_ind(ind_3, 1)
+                list_of_ind_2(ind_temp, 2) = list_of_ind(ind_3, 2)
+                write(*,*) ind_3, list_of_ind_2(ind_temp, 1), list_of_ind_2(ind_temp, 2)
+                ind_temp = ind_temp+1
+            end if
+        end do
+        
+        deallocate (list_of_ind, list_of_ind_bool)
+        allocate( ia( Sz_subspace_size+1), ja(ja_val_arr_size), val_arr(ja_val_arr_size) )
+        !ia and ja inside list_of_ind_2
+        
+        !asynchronous filling of val_arr!
+         !$OMP PARALLEL DO PRIVATE(ind_3, ind_i, ind_j, H_full_ij, omp_id) &
+                          SHARED(ja_val_arr_size, hash_Sz, list_of_ind_2, N_spin, J_spin, val_arr, ja) &
+                          default(none)
+        do ind_3 = 1, ja_val_arr_size      
+            omp_id = omp_get_thread_num()
+            ind_i = hash_Sz( list_of_ind_2(ind_3, 1) )
+            ind_j = hash_Sz( list_of_ind_2(ind_3, 2) )
+            call H_XXX_filling(N_spin, J_spin, ind_i, ind_j, H_full_ij)
+            write(*,*) 'id ', omp_id , 'ind_3', ind_3, ind_i, ind_j, H_full_ij
+            val_arr(ind_3) = H_full_ij
+            ja(ind_3) = list_of_ind_2(ind_3, 2)
+        end do
+        !$OMP END PARALLEL DO 
+        
+        ! loop to calculate ia()
+        ind_temp = 1
+        ind_temp_2 = 1
+        do ind_3 = 1, Sz_subspace_size                
+            ia(ind_3) = ind_temp
+            DO WHILE ( list_of_ind_2(ind_temp_2, 1) == ind_3 ) 
+                ind_temp = ind_temp+1
+                ind_temp_2 = ind_temp_2 + 1
+            END DO 
+        end do
+        ia(Sz_subspace_size +1) = ind_temp
+        
+        write(*,*) 'ia: '
+        do ind_3 = 1, Sz_subspace_size+1
+            write(*,*) ind_3, ia(ind_3)    
+        end do
+        
+        write(*,*) 'ja and val_arr:'
+         do ind_3 = 1, ja_val_arr_size
+            write(*,*) ind_3, ja(ind_3), val_arr(ind_3)
+        end do
+
+        !FEAST diagonalization
+
+        !n=n     ! Sets the size of the problem
+        !a=non_zero_array     ! Array containing the nonzero elements of the upper triangular part of the matrix A
+        call feastinit (fpm)  ! function specifying default parameters fpm of FEAST algorithm
+        fpm(1) = 1
+        fpm(2) = 12 !can be more, can be less
+        fpm(3) = 8 !eps 10^-fpm(3)
+        fpm(4) = 20 ! max number of feast loops
+        fpm(5) = 0 !initial subspace
+        fpm(6) = 0! stopping criterion
+        fpm(7) = 5 !Error trace sigle prec stop crit
+        fpm(14) = 0 ! standard use of feast
+        fpm(27) = 1 !check input matrices
+        fpm(28) = 1 !check if B is positive definite?         
+        uplo='U' ! If uplo = 'U', a stores the upper triangular parts of A.
+        emin = -20.0d0 ! The lower ... &
+        emax =  20.0d0  !  and upper bounds of the interval to be searched for eigenvalues
+        m0 = Sz_subspace_size !On entry, specifies the initial guess for subspace dimension to be used, 0 < m0?n. 
+           !Set m0 ? m where m is the total number of eigenvalues located in the interval [emin, emax]. 
+           !If the initial guess is wrong, Extended Eigensolver routines return info=3.
+        n = Sz_subspace_size
+        allocate( x(n,m0), e(m0), res(m0) )
+        write(*,*) 'Windows 11 new feature: feast might work only for Relase, not Debug!'
+        write(*,*) 'Before dfeast_scsrev... '
+        call dfeast_scsrev(uplo, n, val_arr, ia, ja, fpm, epsout, loop, emin, emax, m0, e, x, m, res, info)
+        write(*,*) 'eps_out= ', epsout
+        write(*,*) 'loop= ', loop
+        write(*,*) ' dfeast_scsrev info=', info
+        write(*,*) 'After  dfeast_scsrev... '
+        
+        if (info /= 0) then
+           write(*,*) 'problem with  dfeast_scsrev, info=', info
+        end if 
+    
+        write(*,*) ' dfeast_scsrev eigenvalues found= ', m
+        do i = 1, m
+            write(*,*) i, e(i)
+        end do
+    
+        write(*,*) ' dfeast_scsrev eigenvec norm:'
+        do i=1,m
+            norm = 0.0d0
+            do j=1, n
+                norm = norm + x(j,i)*(x(j,i))
+            end do
+            write(*,*) i, norm
+        end do
+                
+        deallocate( val_arr, ia, ja, x, e, res)
+        deallocate( list_of_ind_2 )
+        deallocate( open_mp_counter )
+        
+        close(10)
+    end subroutine H_XXX_subspace_fill_and_diag_fast
+
+
+
     subroutine H_create_basis_sz_sorted(N_spin, indices_Sz_basis_sorted)
         !> Returns a binary representation of basis(i) as a string with at least N_spin bits. 
         !> abs(d) < 2^52
